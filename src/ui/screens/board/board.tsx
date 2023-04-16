553692e1d736a78f2fce071591e2fca0c13b484e
nathan <nathanberger.rs@hotmail.com>
2023-04-16 00:46:44 -0300

    style: align board on center of screen

--- src/ui/screens/board/board.tsx
+++ src/ui/screens/board/board.tsx
@@ -5,9 +5,11 @@ import { HousePiece } from '../../components/housePiece/housePiece';
 
 export const Board = () => {
   return (
-    <div className="board-container">
-      {boardPositions.map(line => line.map(house => <HousePiece position={house.value} color={house.color} />))}
-    </div >
+    <div className='board-screen'>
+      <div className="board-container">
+        {boardPositions.map(line => line.map(house => <HousePiece position={house.value} color={house.color} />))}
+      </div >
+    </div>
   );
 }
 
