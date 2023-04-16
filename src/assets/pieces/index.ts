4b194044352a36001cb9241339b3cdfdfb823451
nathan <nathanberger.rs@hotmail.com>
2023-04-16 17:23:13 -0300

    feat: adding board pieces assets

---
+++ src/assets/pieces/index.ts
@@ -0,0 +1,32 @@
+import PawnWhite from './pawn-white.png'
+import PawnBlack from './pawn-black.png'
+
+import BishopWhite from './bishop-white.png'
+import BishopBlack from './bishop-black.png'
+
+import KingWhite from './king-white.png'
+import KingBlack from './king-black.png'
+
+import KnightWhite from './knight-white.png'
+import KnightBlack from './knight-black.png'
+
+import QueenWhite from './queen-white.png'
+import QueenBlack from './queen-black.png'
+
+import RookWhite from './rook-white.png'
+import RookBlack from './rook-black.png'
+
+export {
+  PawnWhite,
+  PawnBlack,
+  BishopWhite,
+  BishopBlack,
+  KingWhite,
+  KingBlack,
+  KnightWhite,
+  KnightBlack,
+  QueenWhite,
+  QueenBlack,
+  RookWhite,
+  RookBlack
+}
