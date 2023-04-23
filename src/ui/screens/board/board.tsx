d4877838e17be2c3a8f24c2c872b8dbb3a634f41
nathan <nathanberger.rs@hotmail.com>
2023-04-23 00:29:08 -0300

    feat(board): add border positions around the board

--- src/ui/screens/board/board.tsx
+++ src/ui/screens/board/board.tsx
@@ -2,17 +2,54 @@ import React from 'react'
 
 import './index.css'
 
-import { boardPositions } from '../../../constants/index.js'
+import {
+  boardPositions,
+  horizontalBoardPositons,
+  verticalBoardPositions
+} from '../../../constants/index.js'
+
 import { HousePiece } from '../../components/housePiece/housePiece'
 
 export const Board: React.FC = () => {
   return (
-    <div className='board-screen'>
-      <div className='board__border--thick'>
-        <div className='board__border--thin'>
-          <div className="board-container">
-            {boardPositions.map(line => line.map((house, index) => <HousePiece key={index} piece={house.icon} color={house.color} />))}
-          </div >
+    <div className="board-screen">
+      <div className="board__border">
+        <div className="board__border--horizontal--thick">
+          <div className="board__border--top">
+            {horizontalBoardPositons.map((position, index) => (
+              <div key={index}>{position}</div>
+            ))}
+          </div>
+          <div className="board__border--vertical--thick">
+            <div className="board__border--left">
+              {verticalBoardPositions.map((position, index) => (
+                <div key={index}>{position}</div>
+              ))}
+            </div>
+            <div className="board__border--thin">
+              <div className="board-container">
+                {boardPositions.map(line =>
+                  line.map((house, index) => (
+                    <HousePiece
+                      key={index}
+                      piece={house.icon}
+                      color={house.color}
+                    />
+                  ))
+                )}
+              </div>
+            </div>
+            <div className="board__border--right">
+              {verticalBoardPositions.map((position, index) => (
+                <div key={index}>{position}</div>
+              ))}
+            </div>
+          </div>
+          <div className="board__border--bottom">
+            {horizontalBoardPositons.map((position, index) => (
+              <div key={index}>{position}</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
