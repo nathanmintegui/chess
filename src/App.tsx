3da182b997d3bce8f5a4562fc4de740b0b397546
nathan <nathanberger.rs@hotmail.com>
2023-04-25 23:22:50 -0300

    remove globalContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import React from 'react'
 import './App.css'
 import { Board } from './ui/screens/board/board'
 
-import { GlobalContextProvider } from './context/useGlobalContext'
 import { GlobalBoardProvider } from './context/useGlobalBoard'
 
 function App() {
   return (
     <GlobalBoardProvider>
-      <GlobalContextProvider>
-        <Board />
-      </GlobalContextProvider>
+      <Board />
     </GlobalBoardProvider>
   )
 }
--- src/context/useGlobalContext.ts
+++
@@ -1,9 +0,0 @@
-import createGlobalState from 'react-create-global-state'
-
-const initialState = { initialPosition: null, finalPosition: null }
-
-const [useGlobalContext, Provider] = createGlobalState(initialState)
-
-export const GlobalContextProvider = Provider
-
-export default useGlobalContext
