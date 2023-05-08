7c74450ec681c57fcdf89e11ebc312b7178cf1d9
nathan <nathanberger.rs@hotmail.com>
2023-05-07 21:38:11 -0300

    feat(pieceMotion): split each pieceMotion'function into separate folders

---
+++ src/pieceMotion/pieces/index.ts
@@ -0,0 +1,6 @@
+export { returnPawnPossibleMovimentPositions } from './pawn'
+export { returnRookPossibleMovimentPositions } from './rook'
+export { returnQueenPossibleMovimentPositions } from './queen'
+export { returnKingPossibleMovimentPositions } from './king'
+export { returnKnightPossibleMovimentPositions } from './knight'
+export { returnBishopPossibleMovimentPositions } from './bishop'
---
+++ src/pieceMotion/pieces/queen.ts
@@ -0,0 +1,5 @@
+export const returnQueenPossibleMovimentPositions = (
+  position: string
+): string[] => {
+  return []
+}
