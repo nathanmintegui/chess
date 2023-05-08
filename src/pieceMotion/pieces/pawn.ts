1d70dd0fbbd8acff25f23715e3d429535a4c4a9f
nathan <nathanberger.rs@hotmail.com>
2023-05-07 22:45:27 -0300

    feat(pawn): add available positions for moviment

--- src/pieceMotion/pieces/pawn.ts
+++ src/pieceMotion/pieces/pawn.ts
@@ -1,5 +1,23 @@
 export const returnPawnPossibleMovimentPositions = (
-  position: string
+  pieceposition: string
 ): string[] => {
-  return []
+  const possiblePositions: string[] = []
+
+  const letter: string = pieceposition[0]
+  const number: number = Number(pieceposition[1])
+
+  let firstPossibility: string = ''
+  let secondPossibility: string = ''
+
+  if (number === 2) {
+    firstPossibility = `${letter}${number + 1}`
+    secondPossibility = `${letter}${number + 2}`
+
+    possiblePositions.push(firstPossibility, secondPossibility)
+  } else {
+    firstPossibility = `${letter}${number + 1}`
+    possiblePositions.push(firstPossibility)
+  }
+
+  return possiblePositions
 }
